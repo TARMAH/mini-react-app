52efae540647f0d2bd778c6fd071f8894acbe160
Tarmah <tiqbal.bscs16seecs@seecs.edu.pk>
2020-03-11 23:21:42 +0500

    Updates

---
+++ precache-manifest.e85c189fd9ae4066e830c39417e1d954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d095f5ecfa2bfd447ff729cb834eaf1",
+    "url": "/mini-react-app/index.html"
+  },
+  {
+    "revision": "cd3a58979aff2d8f89ab",
+    "url": "/mini-react-app/static/css/main.98fb0fed.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/mini-react-app/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/mini-react-app/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3a58979aff2d8f89ab",
+    "url": "/mini-react-app/static/js/main.c276ec64.chunk.js"
+  },
+  {
+    "revision": "85fdbd0767fa187f129c",
+    "url": "/mini-react-app/static/js/runtime-main.a1d38d7c.js"
+  }
+]);
